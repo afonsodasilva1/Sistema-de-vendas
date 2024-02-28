38c591f870fcf3ecf5cae5f3f71d0e13ed1503eb
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2024-02-28 13:21:59 +0100

    fix (src): Couldn't conect database to the server

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,25 @@ import { Module } from '@nestjs/common';
 import { ClientModule } from './modules/client.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { PostgresConfigService } from './config/postgres.config.service';
 
 @Module({
-  imports: [
-    ClientModule, 
-    ConfigModule.forRoot({
-      isGlobal: true
-    })
+  imports: [ 
+    ConfigModule.forRoot()
     ,
-    TypeOrmModule.forRootAsync({
-      useClass: PostgresConfigService,
-      inject: [PostgresConfigService],
-    })
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5433,
+      username: "postgres",
+      password: "945332002Af*",
+      database: "sales_db",
+      synchronize: true,
+      entities: [`${__dirname}/**/models/*.{ts, js}`], 
+      migrations: [`${__dirname}/**/migrations/*.{ts, js}`], 
+    }),
+    ClientModule
   ],
+
 })
 export class AppModule {}
+
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('/api-vendas')
+  app.setGlobalPrefix('/api-sales')
 
   await app.listen(3333);
 }
